8f82e6ef9212b0408e27717afcf0c60e700f7086
Amikor <55d55r77@gmail.com>
2025-10-04 12:02:03 +0000

    add product_router in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from src.api.products import router as products_router
 
 app = FastAPI(title="MyShop", version="0.0.1")
 
@@ -7,6 +8,8 @@ app = FastAPI(title="MyShop", version="0.0.1")
 @app.get("/", tags=["Магазин"])
 async def home():
     return {"data" : "Welcome to my Shop"}
+
+app.include_router(products_router)
 
 
 
