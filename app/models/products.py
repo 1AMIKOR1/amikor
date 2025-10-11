a02f03a1f86aaf46e063a6e5becf6b0ad6a15cdc
Amikor <55d55r77@gmail.com>
2025-10-11 11:28:46 +0300

    add product model

---
+++ app/models/products.py
@@ -0,0 +1,24 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from typing import TYPE_CHECKING
+
+from app.database import Base
+
+if TYPE_CHECKING:
+    from app.models.manufacturers import ManufacturersModel
+    
+class RoomsModel(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    manufacture_id: Mapped[int] = mapped_column(ForeignKey("manufactures.id"))
+    title: Mapped[str] = mapped_column(String(100))
+    price: Mapped[int]
+    quantity: Mapped[int]
+
+    manufacturere: Mapped["ManufacturersModel"] = relationship(
+        back_populates="products",
+
+    )
+ 
