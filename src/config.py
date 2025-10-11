49b5a42dd024f6125e8938c4cbba78d2bbb9a4f6
Amikor <55d55r77@gmail.com>
2025-10-11 13:25:00 +0300

    add alembic config

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+class Settings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    DB_NAME:str
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+
+    )
+    @property
+    def get_db_url(self):
+        return ('sqlite+aiosqlite:///{self.DB_NAME}')
+    @property
+    def auth_data(self):
+        return {"secret_key": self.SECRET_KEY, "algorithm": self.ALGORITHM}
+settings = Settings()
