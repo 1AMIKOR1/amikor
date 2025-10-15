f2ffce68fe4df80ff067b93382f092aa870d2b86
Amikor <55d55r77@gmail.com>
2025-10-15 18:45:22 +0300

    add models

---
+++ src/models/manufacturers.py
@@ -0,0 +1,13 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class ManufacturersModel(Base):
+    __tablename__ = "manufacturers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(50))
+ 
