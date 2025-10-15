f69e3b4ec9400f4e9a411de9ab934b755cb84f6e
Amikor <55d55r77@gmail.com>
2025-10-15 18:36:52 +0300

    add database.py

---
+++ src/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from src.config import settings
+
+engine = create_async_engine(settings.get_db_url)
+
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
