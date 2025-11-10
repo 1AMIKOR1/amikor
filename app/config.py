fece023dac5912d6b9401c43c37aaee8b2ff2182
Rokima <55d55r77@gmail.com>
2025-11-10 10:47:46 +0300

    add auth service

--- app/config.py
+++ app/config.py
@@ -1,19 +1,22 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )
+
     @property
     def db_url(self):
-        return ('sqlite+aiosqlite:///db.sqlite3')
+        return "sqlite+aiosqlite:///db.sqlite3"
+
     @property
     def auth_data(self):
         return {"secret_key": settings.SECRET_KEY, "algorithm": settings.ALGORITHM}
 
+
 settings = Settings()
-
-
---
+++ app/services/auth.py
@@ -0,0 +1,72 @@
+from datetime import datetime, timezone, timedelta
+
+from app.config import settings
+from app.exceptions.auth import (
+    UserAlreadyExistsError,
+    UserNotFoundError,
+    InvalidPasswordError,
+)
+from app.schemas.users import SUserAdd, SUserAddRequest, SUserGet
+from app.services.base import BaseService
+import jwt
+from passlib.context import CryptContext
+
+
+class InvalidJWTTokenError:
+    pass
+
+
+class AuthService(BaseService):
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+    def create_access_token(self, data: dict) -> str:
+        to_encode = data.copy()
+        expire: datetime = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+        to_encode |= {"exp": expire}
+        encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
+        return encoded_jwt
+
+    def verify_password(self, plain_password, hashed_password) -> bool:
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def hash_password(self, plain_password) -> str:
+        return self.pwd_context.hash(plain_password)
+
+    def decode_token(self, token: str) -> dict:
+        try:
+            return jwt.decode(token, settings.JWT_SECRET_KEY, [settings.JWT_ALGORITHM])
+        except jwt.exceptions.DecodeError as ex:
+            raise InvalidJWTTokenError from ex
+
+    async def register_user(self, user_data: SUserAddRequest):
+        user = await self.db.users.get_one_or_none(email=user_data.email)
+        if user:
+            raise UserAlreadyExistsError
+        hashed_password: str = AuthService().hash_password(user_data.password)
+        new_user_data = SUserAdd(
+            email=user_data.email, hashed_password=hashed_password, name=user_data.name
+        )
+        await self.db.users.add(new_user_data)
+        await self.db.commit()
+
+    async def login_user(self, user_data: SUserAddRequest):
+        user = await self.db.users.get_user_with_hashed_password(email=user_data.email)
+        if not user:
+            raise UserNotFoundError
+        if not self.verify_password(user_data.password, user.hashed_password):
+            raise InvalidPasswordError
+        access_token: str = self.create_access_token(
+            {
+                "user_id": user.id,
+                "role": user.role,
+            }
+        )
+        return access_token
+
+    async def get_me(self, user_id: int):
+        user: None | SUserGet = await self.db.users.get_one_or_none(id=user_id)
+        if not user:
+            raise UserNotFoundError
+        return user
