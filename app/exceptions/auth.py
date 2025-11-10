4a1e5739e6b9e9c423b8fa3c291e88bf16570898
Rokima <55d55r77@gmail.com>
2025-11-10 10:48:24 +0300

    add auth exceptions

---
+++ app/exceptions/auth.py
@@ -0,0 +1,42 @@
+from app.exceptions.base import MyAppError, MyAppHTTPError
+
+
+class UserAlreadyExistsError(MyAppError):
+    detail = "Пользователь с таким email уже существует"
+
+
+class InvalidJWTTokenError(MyAppError):
+    detail = "Неверный токен"
+
+
+class InvalidPasswordError(MyAppError):
+    detail = "Неверный пароль"
+
+
+class UserNotFoundError(MyAppError):
+    detail = "Пользователя не существует"
+
+
+class InvalidTokenHTTPError(MyAppHTTPError):
+    status_code = 401
+    detail = "Неверный токен доступа"
+
+
+class NoAccessTokenHTTPError(MyAppHTTPError):
+    detail = "Вы не предоставили токен доступа"
+    status_code = 401
+
+
+class UserAlreadyExistsHTTPError(MyAppHTTPError):
+    status_code = 409
+    detail = "Пользователь с таким email уже существует"
+
+
+class UserNotFoundHTTPError(MyAppHTTPError):
+    status_code = 401
+    detail = "Пользователя не существует"
+
+
+class InvalidPasswordHTTPError(MyAppHTTPError):
+    status_code = 401
+    detail = "Неверный пароль"
