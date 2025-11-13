d261eb20605367e35077413e8f859cca931b6436
Amikor <55d55r77@gmail.com>
2025-11-13 23:39:11 +0000

    add new migration

--- migration/env.py
+++ migration/env.py
@@ -7,9 +7,11 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from app.config import settings
 from alembic import context
 from app.database.database import Base
+from app.models.roles import RolesModel
 from app.models.users import UsersModel
 from app.models.products import ProductsModel
 from app.models.manufacturers import ManufacturersModel
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
